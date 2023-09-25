9217c82a9d65dd59e9129e37afd1f49ac2823821
Abdulla-Bin-Islam <abd679405@gmail.com>
2023-09-25 21:31:06 +0600

    start with html

---
+++ lab2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lab2</title>
+</head>
+<body>
+    <center>
+    <fieldset style="width: 100px;" >
+        <h1 align="center" >Login</h1><br>
+        Username: <input type="text"><br>
+        <hr align="left" width="300" ><br>
+        Password: <input type="password"><br>
+        <hr align="left" width="300" ><br>
+        <center>
+            <button>Submit</button>
+        </center>
+    </fieldset><br>
+    </center>
+    
+
+    
